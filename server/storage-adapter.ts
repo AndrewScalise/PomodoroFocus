428c8c14e5ae4d08e3b9b2f3e2331b466f01418c
Andrew Scalise <paradigmcomplex@gmail.com>
2025-05-13 19:06:13 -0600

    removed api stuff that wasn't needed

--- server/storage-adapter.ts
+++
@@ -1,8 +0,0 @@
-// This file adapts the storage interface for Vercel's serverless environment
-import { ServerlessStorage, serverlessStorage } from '../lib/serverless-storage';
-import { IStorage } from './storage';
-
-// Export the appropriate storage implementation
-export const storage: IStorage = process.env.VERCEL 
-  ? serverlessStorage 
-  : require('./storage').storage;
